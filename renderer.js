5aa85cee3128666de1ac223f9f67a2cb52eb9358
oreste-abizera <oresteabizera11@gmail.com>
2022-02-06 13:49:31 +0200

    feat: adding countdown

--- renderer.js
+++ renderer.js
@@ -4,3 +4,35 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+// Set the date we're counting down to
+var minutesToAdd = 30;
+var currentDate = new Date();
+var countDownDate = new Date(
+  currentDate.getTime() + minutesToAdd * 60000
+).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="countdown"
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Link Sent on your email";
+  }
+}, 1000);
